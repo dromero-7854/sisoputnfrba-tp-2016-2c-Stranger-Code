9f7f5a364911b36f3968c3a75ff74ae76056af3c
dromero-7854 <rl.dante@gmail.com>
2016-08-29 21:32:26 -0300

    Eclipse project

---
+++ hola-mundo.c
@@ -0,0 +1,13 @@
+/*
+ * hola-mundo.c
+ *
+ *  Created on: 29/8/2016
+ *      Author: utnso
+ */
+#include<stdio.h>
+#include<stdlib.h>
+int main(void) {
+	printf("Hola mundo");
+	return EXIT_SUCCESS;
+}
+
